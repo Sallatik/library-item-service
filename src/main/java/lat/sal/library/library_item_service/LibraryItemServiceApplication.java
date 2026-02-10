ea688ef4006d5e500cbe5d567ae8c3c38317b10e
sallat <sallat@protonmail.com>
2026-02-10 01:02:20 +0100

    create spring project

---
+++ src/main/java/lat/sal/library/library_item_service/LibraryItemServiceApplication.java
@@ -0,0 +1,13 @@
+package lat.sal.library.library_item_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryItemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lat/sal/library/library_item_service/LibraryItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lat.sal.library.library_item_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
